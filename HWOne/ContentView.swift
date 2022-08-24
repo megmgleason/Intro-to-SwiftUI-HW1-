b00ff744c6ba6ff08853121278e51d4b127eb568
megmgleason <53867050+megmgleason@users.noreply.github.com>
2022-08-24 16:18:16 -0400

    Initial Commit

---
+++ HWOne/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HWOne
+//
+//  Created by MEG G on 8/24/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HWOne/HWOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  HWOneApp.swift
+//  HWOne
+//
+//  Created by MEG G on 8/24/22.
+//
+
+import SwiftUI
+
+@main
+struct HWOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
